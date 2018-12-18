4bef724cd3994b179eb0f3ad8e0131b7a40d8b18
wujunze <itwujunze@163.com>
2018-12-18 13:58:47 +0800

    add some thing

--- src/index.js
+++ src/index.js
@@ -6,7 +6,17 @@ $.ready(function (error) {
         return;
     }
 
-    $('#led-r').turnOn();
+    // 在 `#button` 按下时点亮 `#led-r`.
+    $('#button').on('push', function () {
+        console.log('Button pushed.');
+        $('#led-r').turnOn();
+    });
+
+    // 在 `#button` 释放时熄灭 `#led-r`.
+    $('#button').on('release', function () {
+        console.log('Button released.');
+        $('#led-r').turnOff();
+    });
 });
 
 $.end(function () {
